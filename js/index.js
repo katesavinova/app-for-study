6a5e8456a9319b02bcf7efc5f29e8d4c96fdb7d3
Ekaterina <savinova.katerina@inbox.ru>
2020-10-29 16:08:20 +0400

    added js file with new card-'game'

---
+++ js/index.js
@@ -0,0 +1,82 @@
+let Anser = ['Orange','Banana','Cherry','Pear','Melon','Kiwi','Coconut','Lime','Mango','Peach'];
+let prompt=['Слово начинается с буквы - О'];
+let math=0, count=0, elem, elemText, articleDiv,elem1, elemText1, articleDiv1,elem2, elemText2, articleDiv2;
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    
+     math=Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(math) ; //Максимум и минимум включаются
+    switch(math){
+        case 0:
+             articleDiv = document.querySelector("div.cardAnserText");//куда помещаем
+            // создаем элемент
+             elem = document.createElement("p");
+            // создаем для него текст
+             elemText = document.createTextNode("Какой фрукт (из семейства цитрусовых) ораньжевого цвета?");
+            // добавляем текст в элемент в качестве дочернего элемента
+            elem.appendChild(elemText);
+            // добавляем элемент в блок div
+            articleDiv.appendChild(elem);
+           
+            break;
+        
+    }
+    
+    return math;
+}
+let num1 = document.getElementById('cardAnserBTN');
+  {
+    function ClickAnser(){
+        
+        if(num1.value==Anser[math]) {
+          console.log("yes");
+          elemText.textContent='';
+          alert("Верно!");
+          getRandomIntInclusive(0,1);
+        }
+        else{
+            console.log("no");
+            count++;
+             articleDiv1 = document.querySelector("div.cardAnser_try");//куда помещаем
+            
+           if(count==1){
+            // создаем элемент
+            elem1 = document.createElement("p");
+            // создаем для него текст
+             elemText1 = document.createTextNode("Попытка № "+count);
+            // добавляем текст в элемент в качестве дочернего элемента
+            elem1.appendChild(elemText1);
+            // добавляем элемент в блок div
+            articleDiv1.appendChild(elem1);
+           }
+           if(count==2){
+            elemText1.textContent='Попытка № 2';
+           }
+           if(count==3){
+            elemText1.textContent='Попытка № 3';
+             articleDiv2 = document.querySelector("div.cardAnser_prompt");//куда помещаем
+            // создаем элемент
+             elem2 = document.createElement("p");
+            // создаем для него текст
+             elemText2 = document.createTextNode(prompt[math]);
+            // добавляем текст в элемент в качестве дочернего элемента
+            elem2.appendChild(elemText2);
+            // добавляем элемент в блок div
+            articleDiv2.appendChild(elem2);
+           }
+           if(count==4){
+            elemText.textContent='';
+            elemText1.textContent='';
+            elemText2.textContent='';
+            
+            
+            alert("Это был  "+Anser[math]);
+            count=0;
+            getRandomIntInclusive(0,1);
+           }
+            
+        }
+      }
+  }
+  getRandomIntInclusive(0,1);
---
+++ js/main.js
@@ -0,0 +1,118 @@
+function check()
+      {
+      var yes=0;
+      var no=0;
+      var otvet="";
+      var choice;
+      for (var v=1; v<=3; v++)
+      {
+      var q = document.forms['quiz'].elements['vopros'+v];
+       for (var i=0; i<q.length; i++)
+       {
+       if (q[i].checked) {
+       choice=q[i].value;
+       }
+       }
+       if (choice=="yes") {yes++;}
+       if (choice=="no") {no++;}
+      }
+      switch (true) {
+      case (yes<2): otvet="оценка 3";break;
+      case (yes==2): otvet="оценка 4";break;
+      case (yes>2): otvet="оценка 5";break;
+      default: otvet="очень плохо";break;
+      }
+      alert(otvet);
+      }
+      {
+      const dragstart = function(event) {
+        event.dataTransfer.setData("text", event.target.id);
+      };
+      const dragover = function(event) {
+        if(event.target.nodeName.toLowerCase() === "img") {
+          return true;
+        }
+        event.preventDefault();
+      }
+      const drop = function(event) {
+        event.preventDefault();
+        let imageId = event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(imageId));
+      };
+
+      const cells = document.getElementsByClassName("Acol");
+      Array.from(cells).forEach((element) => {
+        element.addEventListener('dragover',dragover);
+        element.addEventListener('drop',drop);
+      });      
+
+      const images = document.getElementsByTagName("img");
+      Array.from(images).forEach((element) => {
+        element.addEventListener('dragstart',dragstart);
+      });
+    }
+    {
+      function Submit(){
+        if((document.getElementById('EngWord').value)=="Собака") {
+          return alert("Верно!");
+        }
+        else{
+          return alert("Ошибка!Попробуй еще раз.");
+        }
+        
+      }
+      function Submit2(){
+        if((document.getElementById('EngWord2').value)=="Кошка") {
+          return alert("Верно!");
+        }
+        else{
+          return alert("Ошибка!Попробуй еще раз.");
+        }
+      }
+      function Submit3(){
+        if((document.getElementById('EngWord3').value)=="Корова") {
+          return alert("Верно!");
+        }
+        else{
+          return alert("Ошибка!Попробуй еще раз.");
+        }
+       
+        
+      }
+      {
+        function Submit4(){
+          return alert("Верно!");
+          
+        }
+        
+        function Submit5(){
+          return alert("Не верно!");
+        }
+      }
+      {
+        function SubmitFalse(){
+          document.getElementById('cardCick').classList.add('cardCick-bgFalse'); 
+          
+        }
+        
+        function SubmitTrue(){
+          document.getElementById('cardCick').classList.add('cardCick-bgTrue');
+        }
+        function SubmitFalse2(){
+          document.getElementById('cardCick2').classList.add('cardCick-bgFalse'); 
+          
+        }
+        
+        function SubmitTrue2(){
+          document.getElementById('cardCick2').classList.add('cardCick-bgTrue');
+        }
+        function SubmitFalse3(){
+          document.getElementById('cardCick3').classList.add('cardCick-bgFalse'); 
+          
+        }
+        
+        function SubmitTrue3(){
+          document.getElementById('cardCick3').classList.add('cardCick-bgTrue');
+        }
+      }
+    }
